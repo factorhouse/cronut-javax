f89da43cf06f419f6aef0cf20d2ea34e1e81f156
Derek Troy-West <derek@factorhouse.io>
2026-09-24 16:55:07 +1000

    bump deps

--- project.clj
+++ project.clj
@@ -9,15 +9,15 @@
 
   :plugins [[dev.weavejester/lein-cljfmt "0.15.3"]]
 
-  :dependencies [[org.clojure/clojure "1.12.4"]
+  :dependencies [[org.clojure/clojure "1.12.6"]
                  [org.clojure/tools.logging "1.3.1"]
                  [org.quartz-scheduler/quartz "2.4.1" :exclusions [org.slf4j/slf4j-api]]]
 
   :profiles {:dev   {:resource-paths ["dev-resources"]
                      :dependencies   [[ch.qos.logback/logback-classic "1.3.16"]
-                                      [org.slf4j/slf4j-api "2.0.17"]
+                                      [org.slf4j/slf4j-api "2.0.20"]
                                       [org.clojure/core.async "1.9.865"]
-                                      [clj-kondo "2026.01.19" :exclusions [org.clojure/tools.reader]]]}
+                                      [clj-kondo "2026.08.04" :exclusions [org.clojure/tools.reader]]]}
              :smoke {:pedantic? :abort}}
 
   :aliases {"check"  ["with-profile" "+smoke" "check"]
